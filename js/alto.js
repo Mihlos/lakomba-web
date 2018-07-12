cace62b1907dad2d84c05f8cfd565b035428ac85
Mihlos <miguel.mish@gmail.com>
2018-07-12 19:01:51 +0200

    con interior y patio

--- js/alto.js
+++ js/alto.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	$("#particles-js").css({"height":$(window).height() + "px"});
 	$("#ambiente").css({"height":$(window).height()+ "px"});
+	$("#interior").css({"height":$(window).height()+ "px"});
+	$("#patio").css({"height":$(window).height()+ "px"});
+	$("#contacto").css({"height":$(window).height()+ "px"});
+
 });
